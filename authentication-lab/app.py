fec49d2e16e5804ef2fe189ed00e1e759bbdf718
mirnaqu23-meet <109899438+mirnaqu23-meet@users.noreply.github.com>
2022-08-01 18:35:23 +0300

    im done with my authentication lab

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -9,40 +9,54 @@ config = {
   "storageBucket": "fir-160ae.appspot.com",
   "messagingSenderId": "531779820040",
   "appId": "1:531779820040:web:3326b20b3c332e38c54fbd",
-  "measurementId": "G-Z33RMDTMYB"
-  "databaseURL" : ""}
+  "measurementId": "G-Z33RMDTMYB",
+  "databaseURL" : ""
+  }
+firebase = pyrebase.initialize_app (config)
+auth = firebase.auth()
+#db= firebase.database()
 
-  firebase = pyrebase initialize_app (config)
-  auth = firebase.auth()
 
-  app = Flask(__name__, template_folder='templates', static_folder='static')
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+			error = "Authentication failed"
+
+	return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    error = ""
-   if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-            login_session['user'] = 
-auth.create_user_with_email_and_password(email, password)
-           return redirect(url_for('home'))
-       except:
-           error = "Authentication failed"
-    return render_template("signup.html")
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+			error = "Authentication failed"
+			return error
+	else:
+		return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+	return render_template("add_tweet.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
